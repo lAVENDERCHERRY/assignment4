a15942c92c7f06ee4a0a58ee5d69565cb0132eec
jmoua123 <nraugnaagmouaa@gmail.com>
2023-10-28 23:24:45 -0700

    complete

--- 4-3.c
+++ 4-3.c
@@ -1,16 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void transposeOpt(long a[4][4]) {
-  for (long i = 0; i < 4; ++i) {
-    long *rp = &a[i][0];
-    long *cp = &a[0][i];
-    for (long j = 0; j < i; ++j) {
-      // TODO - swap data at rp, cp pointers, increment rp, cp
-      // (as shown in the optimized x86-64 code above)
+
+// COPY .L3 ASSEMBLY CODE HERE:
+
+
+
+// Transpose function that swaps elements in a 4x4 matrix
+void transpose(long a[4][4]) {
+    for (long i = 0; i < 4; i++) {
+        for (long j = 0; j < i; j++) {
+            // Store the value of a[i][j] in t1
+            long t1 = a[i][j];
+            // Store the value of a[j][i] in t2
+            long t2 = a[j][i];
+            // Swap the values of a[i][j] and a[j][i]
+            a[i][j] = t2;
+            a[j][i] = t1;
+        }
     }
-  }
 }
+
+// Optimized transpose function using pointers to swap elements in a 4x4 matrix
+void transposeOpt(long a[4][4]) {
+    for (long i = 0; i < 4; ++i) {
+        // Initialize rp (row pointer) to point to the start of the current row
+        long *rp = &a[i][0];
+        // Initialize cp (column pointer) to point to the start of the current column
+        long *cp = &a[0][i];
+        for (long j = 0; j < i; ++j) {
+            // Swap data at rp and cp pointers without using temporary variables
+            long temp = *rp;
+            *rp = *cp;
+            *cp = temp;
+            
+            // Increment rp to move to the next column in the current row
+            rp++;
+            // Increment cp to move to the next row in the current column
+            cp += 4;  // Since it's a 4x4 matrix, you need to move to the next column.
+        }
+    }
+}
+
+
+
+
 void print(long a[4][4]) {
   for (int i = 0; i < 4; ++i) {
     for (int j =  0; j < 4; ++j) {
@@ -34,10 +68,36 @@ int main(int argc, char *argv[]) {
     printf("transpose(a)\n");
     print(a);
     transposeOpt(b);
-    printf("trasnposeOpt(b)\n");
+    printf("transposeOpt(b)\n");
     print(b);
   } else {
-    // TODO - your test cases go here!
+    // Test cases
+
+    // Test case 1: A simple 4x4 matrix
+    long testMatrix1[4][4] = {{1, 2, 3, 4},
+                              {5, 6, 7, 8},
+                              {9, 10, 11, 12},
+                              {13, 14, 15, 16}};
+
+    printf("Original Matrix 1:\n");
+    print(testMatrix1);
+
+    transposeOpt(testMatrix1);
+    printf("Transposed Matrix 1:\n");
+    print(testMatrix1);
+
+    // Test case 2: Another 4x4 matrix
+    long testMatrix2[4][4] = {{11, 12, 13, 14},
+                              {15, 16, 17, 18},
+                              {19, 20, 21, 22},
+                              {23, 24, 25, 26}};
+
+    printf("Original Matrix 2:\n");
+    print(testMatrix2);
+
+    transposeOpt(testMatrix2);
+    printf("Transposed Matrix 2:\n");
+    print(testMatrix2);
   }
   return 0;
 }
